ef392d8acb1c346d584750cae992d8235e99eba5
S. Sanneman <seandafydd@gmail.com>
2020-10-01 11:35:33 -0700

    setting up the server, initializing files structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+
